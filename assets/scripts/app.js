afa557af4d6991063b577dd31a7972e68714b2c7
shashankkatte <shashankanand.katte@gmail.com>
2020-05-27 15:09:48 -0400

    New movie render added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,33 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+       <h2>${title}</h2> 
+       <p>${rating}/5 Stars</p>
+    </div>
+    `;
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -52,6 +77,9 @@ const addMovieHandler = () => {
   movies.push(newMovie);
   console.log(movies);
   toggleMovieModal();
+  renderNewMovieElement(newMovie.title,newMovie.image, newMovie.rating);
+  updateUI();
+  clearMovieInput();
 };
 
 const backdropClickHandler = () => {
